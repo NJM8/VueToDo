e907094b6ddd54934509343224f479a02a14f0c7
Nate Mallison <healthynate@gmail.com>
2018-06-22 06:31:31 -0400

    removing boilerplate code

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,5 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <button class="bg-blue hover:bg-blue-light text-white font-bold py-2 px-4 border-b-4 border-blue-dark hover:border-blue rounded">
-      Button
-    </button>
-    <router-view/>
-  </div>
+  <div id="app"/>
 </template>
 
 <script>
@@ -17,12 +11,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
