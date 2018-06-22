470538eb91f6780dbf9f660091acc6debfd6c919
Nate Mallison <healthynate@gmail.com>
2018-06-22 07:13:58 -0400

    setup basic default card styling, added link to portfolio and flex containers to main app. added empty templates for ListTodo and Todo.

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,22 @@
 <template>
   <div
     id="app"
-    class="app-bg">
-    <h1>Hello</h1>
+    class="app-bg flex flex-col">
+    <div class="max-w-xs app-card">
+      <h1>Vue To Do</h1>
+    </div>
+    <div class="flex-auto">
+      <div class="max-w-lg app-card flex-auto">
+        <p>Router</p>
+      </div>
+    </div>
+    <div class="max-w-sm app-card">
+      <a
+        href="http://www.natethedev.com"
+        target="_blank"
+        rel="noopener"
+        class="no-underline text-black hover:font-bold">Created by Nate Mallison</a>
+    </div>
   </div>
 </template>
 
