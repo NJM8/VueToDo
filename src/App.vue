b39852c43e9dcd00ec23b6c419bf2143d0b66814
Nate Mallison <healthynate@gmail.com>
2018-06-22 09:49:12 -0400

    added dummy data to flush out basic style for displaying todo lists and todos

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,30 @@
     </div>
     <div class="flex-auto">
       <div class="max-w-lg app-card">
-        <p>Router</p>
+        <div class="flex items-stretch">
+          <div
+            id="routerlinks"
+            class="p-2 border-r-2 border-vue-green">
+            <h2>Todo Lists</h2>
+            <ul>
+              <li>list one</li>
+              <li>list two</li>
+              <li>list three</li>
+            </ul>
+            <button>Add List</button>
+          </div>
+          <div
+            id="routerView"
+            class="p-2 text-center flex-auto">
+            <h2>list title</h2>
+            <ul>
+              <li>item one <span>Done</span> <span>Delete</span></li>
+              <li>item two <span>Done</span> <span>Delete</span></li>
+              <li>item three <span>Done</span> <span>Delete</span></li>
+            </ul>
+            <button>add item</button>
+          </div>
+        </div>
       </div>
     </div>
     <div class="max-w-sm app-card">
