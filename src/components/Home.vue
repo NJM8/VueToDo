8d101e6c522a089521d7177cc2121eacfb7c9b79
Nate Mallison <healthynate@gmail.com>
2018-06-22 21:23:23 -0400

    added home component

---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Welcome to Vue To Do</h1>
+    <p>Start by adding a list to the left</p>
+    <p>Then you can add items as needed</p>
+    <p>Enjoy!</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
